hubotio/hubot

commit 07c16db72b79c409c3db3775c2b45cf066b51585

Update to pass the originating room to adapters

src/adapters/campfire.coffee
@@ -6,17 +6,17 @@ Adapter                                 = require '../adapter'
 {TextMessage,EnterMessage,LeaveMessage} = require '../message'
 
 class Campfire extends Adapter
-  send: (user, strings...) ->
+  send: (room, user, strings...) ->
     if strings.length > 0
-      @bot.Room(user.room).speak strings.shift(), (err, data) =>
+      @bot.Room(room).speak strings.shift(), (err, data) =>
         @robot.logger.error "Campfire error: #{err}" if err?
         @send user, strings...
 
-  reply: (user, strings...) ->
-    @send user, strings.map((str) -> "#{user.name}: #{str}")...
+  reply: (room, user, strings...) ->
+    @send room, user, strings.map((str) -> "#{user.name}: #{str}")...
 
-  topic: (user, strings...) ->
-    @bot.Room(user.room).topic strings.join(" / "), (err, data) =>
+  topic: (room, user, strings...) ->
+    @bot.Room(room).topic strings.join(" / "), (err, data) =>
       @robot.logger.error "Campfire error: #{err}" if err?
 
   run: ->

src/adapters/shell.coffee
@@ -5,14 +5,14 @@ Adapter       = require '../adapter'
 {TextMessage} = require '../message'
 
 class Shell extends Adapter
-  send: (user, strings...) ->
+  send: (room, user, strings...) ->
     unless process.platform is 'win32'
       console.log "\x1b[01;32m#{str}\x1b[0m" for str in strings
     else
       console.log "#{str}" for str in strings
     @repl.prompt()
 
-  reply: (user, strings...) ->
+  reply: (room, user, strings...) ->
     strings = strings.map (s) -> "#{user.name}: #{s}"
     @send user, strings...
 

src/adapter.coffee
@@ -8,28 +8,31 @@ class Adapter extends EventEmitter
 
   # Public: Raw method for sending data back to the chat source. Extend this.
   #
+  # room    - A String or Number identifier of the room.
   # user    - A User instance.
   # strings - One or more Strings for each message to send.
   #
   # Returns nothing.
-  send: (user, strings...) ->
+  send: (room, user, strings...) ->
 
   # Public: Raw method for building a reply and sending it back to the chat
   # source. Extend this.
   #
+  # room    - A String or Number identifier of the room.
   # user    - A User instance.
   # strings - One or more Strings for each reply to send.
   #
   # Returns nothing.
-  reply: (user, strings...) ->
+  reply: (room, user, strings...) ->
 
   # Public: Raw method for setting a topic on the chat source. Extend this.
   #
+  # room    - A String or Number identifier of the room.
   # user    - A User instance.
   # strings - One more more Strings to set as the topic.
   #
   # Returns nothing.
-  topic: (user, strings...) ->
+  topic: (room, user, strings...) ->
 
   # Public: Raw method for invoking the bot to run. Extend this.
   #

src/message.coffee
@@ -3,6 +3,7 @@ class Message
   #
   # user - A User instance that sent the message.
   constructor: (@user, @done = false) ->
+    @room = @user.room
 
   # Indicates that no other Listener should be called on this object
   #

src/response.coffee
@@ -7,11 +7,6 @@ class Response
   # message - A Message instance.
   # match   - A Match object from the successful Regex match.
   constructor: (@robot, @message, @match) ->
-    # Dirty hack to deep clone without a dependency...
-    json = JSON.stringify @message
-    message = JSON.parse json
-    @message = message
-
 
   # Public: Posts a message back to the chat source
   #
@@ -20,7 +15,7 @@ class Response
   #
   # Returns nothing.
   send: (strings...) ->
-    @robot.adapter.send @message.user, strings...
+    @robot.adapter.send @message.room, @message.user, strings...
 
   # Public: Posts a message mentioning the current user.
   #
@@ -29,7 +24,7 @@ class Response
   #
   # Returns nothing.
   reply: (strings...) ->
-    @robot.adapter.reply @message.user, strings...
+    @robot.adapter.reply @message.room, @message.user, strings...
 
   # Public: Posts a topic changing message
   #
@@ -38,7 +33,7 @@ class Response
   #
   # Returns nothing.
   topic: (strings...) ->
-    @robot.adapter.topic @message.user, strings...
+    @robot.adapter.topic @message.room, @message.user, strings...
 
   # Public: Picks a random item from the given items.
   #

commit 25d8d7ff599b5cd3535c67d5a01f39063586858f

Fix exception when sending multiple messages
Looks like this has been broken since
07c16db72b79c409c3db3775c2b45cf066b51585, which added a `room` parameter
to `Adapter::send`. dc27aca6fcbd0d5b8d7fdc47f2feeb6638d3a028 changed
that to an `envelope` parameter, but didn't quite fix the bug.

src/adapters/campfire.coffee
@@ -10,7 +10,7 @@ class Campfire extends Adapter
     if strings.length > 0
       @bot.Room(envelope.room).speak strings.shift(), (err, data) =>
         @robot.logger.error "Campfire error: #{err}" if err?
-        @send envelope.user, strings...
+        @send envelope, strings...
 
   reply: (envelope, strings...) ->
     @send envelope, strings.map((str) -> "#{envelope.user.name}: #{str}")...

src/adapters/shell.coffee
@@ -14,7 +14,7 @@ class Shell extends Adapter
 
   reply: (envelope, strings...) ->
     strings = strings.map (s) -> "#{envelope.user.name}: #{s}"
-    @send envelope.user, strings...
+    @send envelope, strings...
 
   run: ->
     self = @