b0e7f8329c27f02732803a5854b263bf5af397f1
Will Pearson <will@mcneel.com>
2017-11-06 12:30:49 +0000

    Set max json request body size with EXPRESS_LIMIT

--- src/robot.js
+++ src/robot.js
@@ -441,7 +441,7 @@ class Robot {
     }
     app.use(express.query())
 
-    app.use(express.json())
+    app.use(express.json({ limit }))
     app.use(express.urlencoded({ limit, parameterLimit: paramLimit }))
     // replacement for deprecated express.multipart/connect.multipart
     // limit to 100mb, as per the old behavior
