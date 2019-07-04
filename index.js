24f0f51465eba803b1e886028b063635ef48da68
Joey Guerra <jguerra@thoughtworks.com>
2019-07-04 15:34:55 -0500

    Updated bin/hubot to remove es2015

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 'use strict'
+const User = require('./src/user')
+const Brain = require('./src/brain')
+const Robot = require('./src/robot')
+const Adapter = require('./src/adapter')
+const Response = require('./src/response')
+const Listener = require('./src/listener')
+const Message = require('./src/message')
+const DataStore = require('./src/datastore')
+module.exports = {
+  User,
+  Brain,
+  Robot,
+  Adapter: Adapter,
+  Response,
+  Listener: Listener.Listener,
+  TextListener: Listener.TextListener,
+  Message: Message.Message,
+  TextMessage: Message.TextMessage,
+  EnterMessage: Message.EnterMessage,
+  LeaveMessage: Message.LeaveMessage,
+  TopicMessage: Message.TopicMessage,
+  CatchAllMessage: Message.CatchAllMessage,
+  DataStore: DataStore.DataStore,
+  DataStoreUnavailable: DataStore.DataStoreUnavailable,
 
-const inherits = require('util').inherits
-
-const hubotExport = require('./es2015')
-
-module.exports = Object.keys(hubotExport).reduce((map, current) => {
-  map[current] = hubotExport[current]
-  return map
-}, {})
+  loadBot (adapterPath, adapterName, enableHttpd, botName, botAlias) {
+    return new module.exports.Robot(adapterPath, adapterName, enableHttpd, botName, botAlias)
+  }
+}
