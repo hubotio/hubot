c08e2f238d405572f08309f38b706c6682721e90
Joey Guerra <github@joeyguerra.com>
2022-08-15 13:08:26 -0500

    remove slack, the client is deprecated

--- src/adapters/slack-mention.mjs
+++
@@ -1,8 +0,0 @@
-class SlackMention {
-  constructor (id, type, info) {
-    this.id = id
-    this.type = type
-    this.info = (info != null) ? info : undefined
-  }
-}
-export default SlackMention
