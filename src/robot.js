154a2cf3bcf1420214f93eed5ba5ebf5bf1210c5
Joey Guerra <github@joeyguerra.com>
2023-04-06 22:35:13 -0500

    fixes #13. Replace  with a different strategy

--- src/robot.js
+++ src/robot.js
@@ -352,7 +352,7 @@ class Robot {
     const full = path.join(filepath, path.basename(filename, ext))
 
     // see https://github.com/hubotio/hubot/issues/1355
-    if (!require.extensions[ext]) { // eslint-disable-line
+    if (['.js', '.mjs'].indexOf(ext) == -1) { // eslint-disable-line
       return
     }
 
