c7cceb2af0a37114f8e41a9b00e160b750dff7d4
Joey Guerra <github@joeyguerra.com>
2023-06-05 07:54:36 -0500

    Remove loading hubot scripts from file

--- bin/hubot.js
+++ bin/hubot.js
@@ -101,7 +101,6 @@ function loadScripts () {
   robot.load(pathResolve('.', 'scripts'))
   robot.load(pathResolve('.', 'src', 'scripts'))
 
-  loadHubotScripts()
   loadExternalScripts()
 
   options.scripts.forEach((scriptPath) => {
@@ -111,70 +110,6 @@ function loadScripts () {
 
     robot.load(pathResolve('.', scriptPath))
   })
-}
-
-function loadHubotScripts () {
-  const hubotScripts = pathResolve('.', 'hubot-scripts.json')
-  let scripts
-  let scriptsPath
-
-  if (fs.existsSync(hubotScripts)) {
-    let hubotScriptsWarning
-    const data = fs.readFileSync(hubotScripts)
-
-    if (data.length === 0) {
-      return
-    }
-
-    try {
-      scripts = JSON.parse(data)
-      scriptsPath = pathResolve('node_modules', 'hubot-scripts', 'src', 'scripts')
-      robot.loadHubotScripts(scriptsPath, scripts)
-    } catch (error) {
-      const err = error
-      robot.logger.error(`Error parsing JSON data from hubot-scripts.json: ${err}`)
-      process.exit(1)
-    }
-
-    hubotScriptsWarning = 'Loading scripts from hubot-scripts.json is deprecated and ' + 'will be removed in 3.0 (https://github.com/github/hubot-scripts/issues/1113) ' + 'in favor of packages for each script.\n\n'
-
-    if (scripts.length === 0) {
-      hubotScriptsWarning += 'Your hubot-scripts.json is empty, so you just need to remove it.'
-      return robot.logger.warning(hubotScriptsWarning)
-    }
-
-    const hubotScriptsReplacements = pathResolve('node_modules', 'hubot-scripts', 'replacements.json')
-    const replacementsData = fs.readFileSync(hubotScriptsReplacements)
-    const replacements = JSON.parse(replacementsData)
-    const scriptsWithoutReplacements = []
-
-    if (!fs.existsSync(hubotScriptsReplacements)) {
-      hubotScriptsWarning += 'To get a list of recommended replacements, update your hubot-scripts: npm install --save hubot-scripts@latest'
-      return robot.logger.warning(hubotScriptsWarning)
-    }
-
-    hubotScriptsWarning += 'The following scripts have known replacements. Follow the link for installation instructions, then remove it from hubot-scripts.json:\n'
-
-    scripts.forEach((script) => {
-      const replacement = replacements[script]
-
-      if (replacement) {
-        hubotScriptsWarning += `* ${script}: ${replacement}\n`
-      } else {
-        scriptsWithoutReplacements.push(script)
-      }
-    })
-
-    hubotScriptsWarning += '\n'
-
-    if (scriptsWithoutReplacements.length > 0) {
-      hubotScriptsWarning += 'The following scripts don’t have (known) replacements. You can try searching https://www.npmjs.com/ or http://github.com/search or your favorite search engine. You can copy the script into your local scripts directory, or consider creating a new package to maintain yourself. If you find a replacement or create a package yourself, please post on https://github.com/github/hubot-scripts/issues/1641:\n'
-      hubotScriptsWarning += scriptsWithoutReplacements.map((script) => `* ${script}\n`).join('')
-      hubotScriptsWarning += '\nYou an also try updating hubot-scripts to get the latest list of replacements: npm install --save hubot-scripts@latest'
-    }
-
-    robot.logger.warning(hubotScriptsWarning)
-  }
 }
 
 function loadExternalScripts () {
